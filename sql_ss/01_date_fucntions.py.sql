a2eae314093a9095c5bb92e8826031a50ff9d20d
sandeepny441 <sandeepny441@gmail.com>
2025-04-10 11:22:18 -0500

    added SQL

---
+++ sql_ss/01_date_fucntions.py.sql
@@ -0,0 +1,8 @@
+-- MySQL / PostgreSQL
+SELECT CURRENT_DATE;
+SELECT CURRENT_TIME;
+SELECT NOW(); -- current date & time
+
+-- SQL Server
+SELECT GETDATE();       -- current date & time
+SELECT CONVERT(DATE, GETDATE());  -- just the date
