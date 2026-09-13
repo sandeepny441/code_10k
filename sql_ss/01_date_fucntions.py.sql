843badb0ed4219e206f844e41a1e7d0b4ac3fad0
sandeep <san@sandeeps-MacBook-Air.local>
2026-09-13 16:40:21 -0400

    added

--- sql_ss/01_date_fucntions.py.sql
+++
@@ -1,8 +0,0 @@
--- MySQL / PostgreSQL
-SELECT CURRENT_DATE;
-SELECT CURRENT_TIME;
-SELECT NOW(); -- current date & time
-
--- SQL Server
-SELECT GETDATE();       -- current date & time
-SELECT CONVERT(DATE, GETDATE());  -- just the date
